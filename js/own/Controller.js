a85359b0df5c0890594547fdf943e4f477ca6309
Stellan Lindell <stiltet@gmail.com>
2013-04-08 19:18:20 +0200

    Created the project and added all the files

---
+++ js/own/Controller.js
@@ -0,0 +1,20 @@
+/// <reference path="../bootstrap.min.js" />
+/// <reference path="../less-1.3.3.min.js" />
+/// <reference path="../modernizr-latest.js" />
+/// <reference path="../jquery/jquery-1.9.1.min.js" />
+/// <reference path="../jquery/jquery-ui-1.10.2.min.js" />
+/// <reference path="../../index.html" />
+/// <reference path="FuctionalityModel.js" />
+/// <reference path="UIModel.js" />
+/*global fuctionalityObject:true*/
+/*global uiObject:true*/
+
+console.log("Controller");
+
+$(document).ready(function () {
+  "use strict";
+  var UI = uiObject();
+  var func = fuctionalityObject();
+
+
+});
---
+++ js/own/UIModel.js
@@ -0,0 +1,13 @@
+/// <reference path="../bootstrap.min.js" />
+/// <reference path="../less-1.3.3.min.js" />
+/// <reference path="../modernizr-latest.js" />
+/// <reference path="../jquery/jquery-1.9.1.min.js" />
+/// <reference path="../jquery/jquery-ui-1.10.2.min.js" />
+
+console.log("UI");
+
+var uiObject = function () {
+  return {
+
+  };
+};
